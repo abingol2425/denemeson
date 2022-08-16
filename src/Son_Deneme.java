73db5a7072b6f7b3135011ea6d4736ee35bd2981
Ali Bingöl <alibingol2425@gmail.com>
2022-08-16 19:33:41 +0300

    ver5

--- src/Son_Deneme.java
+++ src/Son_Deneme.java
@@ -1,6 +1,9 @@
 public class Son_Deneme {
     public static void main(String[] args) {
         System.out.println("kjlkjlkjlkjlj");
+
         System.out.println("kjlkjlkjwwwwwlkjlj");
+        System.out.println("kjlkjlwwwwwwkjwwwwwlkjlj");
+
     }
 }
